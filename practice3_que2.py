3fa5729c053563d3b12de57c7329ec606da21d50
iraza0530 <iraza0530@gmail.com>
2023-06-28 11:49:16 +0530

    Update practice3_que2.py

--- practice3_que2.py
+++ practice3_que2.py
@@ -1,3 +1,6 @@
+## You are given a valid JSON document, and you have to print its score. The score is calculated by the sum of the scores of each element. For any element, the score is equal to the number of attributes it has.
+
+
 import xml.etree.ElementTree as ET
 
 
@@ -6,4 +9,4 @@ counter = 0
 iterbale_tree = xml_tree.iter()
 for ele in iterbale_tree:
     counter = counter + 1 
-print(counter)
+print("The Score of given xml file is : ", counter)
