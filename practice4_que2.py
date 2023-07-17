f70025a7cf7e1661bdd1f1a98c23480e83331151
iraza0530 <iraza0530@gmail.com>
2023-07-17 13:49:53 +0530

    Add files via upload

---
+++ practice4_que2.py
@@ -0,0 +1,57 @@
+def sort_alphanumeric(string):
+    lowercase_letters = []
+    uppercase_letters = []
+    odd_digits = []
+    even_digits = []
+    for c in string:
+        if c.islower():
+            lowercase_letters.append(c)
+        elif c.isupper():
+            uppercase_letters.append(c)
+        elif c.isdigit():
+            if(int(c) % 2 == 0):
+               even_digits.append(c)
+            else:
+                odd_digits.append(c)
+    
+    lowercase_letters.sort()
+    uppercase_letters.sort()
+    odd_digits.sort()
+    even_digits.sort()
+    
+    return ''.join(lowercase_letters + uppercase_letters + odd_digits + even_digits)
+
+
+input_string = input('   Enter the string...')
+print(sort_alphanumeric(input_string))
+
+
+
+
+# Unit tests
+def unit_tests():
+    input_str = "HelloWorld1234"
+    expected_output = "dellloorHW1324"
+    assert sort_alphanumeric(input_str) == expected_output, f"Test case 1 failed"
+    
+    input_str = ""
+    expected_output = ""
+    assert sort_alphanumeric(input_str) == expected_output, f"Test case 2 failed"
+    
+    input_str = "dcba"
+    expected_output = "abcd"
+    assert sort_alphanumeric(input_str) == expected_output, f"Test case 3 failed"
+    
+    input_str = "DCBA"
+    expected_output = "ABCD"
+    assert sort_alphanumeric(input_str) == expected_output, f"Test case 4 failed"
+    
+    input_str = "987654321"
+    expected_output = "135792468"
+    assert sort_alphanumeric(input_str) == expected_output, f"Test case 5 failed"
+    
+    print("All test cases passed successfully!")
+
+
+# Run unit tests
+unit_tests()
---
+++ practice4_que3.py
@@ -0,0 +1,54 @@
+import re
+
+def validate_credit_card(credit_card_number):
+    # Remove hyphens from the card number
+    credit_card_number = credit_card_number.replace('-', '')
+
+    # Check if the card number has exactly 16 digits
+    if len(credit_card_number) != 16:
+        return "Credit Card number should only contain 16 digits..."
+
+    # Check if the card number only consists of digits
+    if not credit_card_number.isdigit():
+        return "Credti Card number should only be NUMERIC..."
+
+    # Check if the card number starts with 4, 5, or 6
+    if credit_card_number[0] not in ['4', '5', '6']:
+        return "Credit Card number should only start with 4 ,5 or 6 ..."
+
+    # Check if the card number has 4 consecutive repeated digits
+    if re.search(r'(.)\1{3}', credit_card_number):
+        return "Invalid Credit card given..."
+
+    return "Valid Credit Card Number given"
+
+
+input_credit_card_number = input("  Enter 16 digit Credit Card Number...")
+
+print(validate_credit_card(input_credit_card_number))
+
+# Unit test cases
+def run_unit_tests():
+    test_cases = [
+        # Valid card numbers
+        ("4567-1234-5678-9012", True),
+        ("4567123456789012", True),
+        ("61234-567-8912-3456", True),
+        ("4123356789123456", True),
+        ("412356789123456", False),  # Less than 16 digits
+        ("5123-4567-8912-3456-", False),  # Invalid separator
+        ("5123-4567-8912-3456-D", False),  # Invalid separator
+        ("5123@4567@8912@3456", False),  # Invalid separator
+        ("41233567891234566", False),  # More than 16 digits
+        ("4123456789123456", False),  # Starts with an invalid digit (1)
+        ("61234-567-8912-3456-9", False),  # More than 16 digits after removing hyphens
+        ("4444444444444444", False),  # Consecutive repeated digits
+    ]
+
+    for credit_card_number, expected_result in test_cases:
+        result = validate_credit_card(credit_card_number)
+        assert result == expected_result
+        print(f"Card Number: {credit_card_number}, Valid: {result}")
+
+# run unit test cases
+run_unit_tests()
